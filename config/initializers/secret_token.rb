383e3f1f316b4e444faaaec2bd9cd283cc102a54
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-19 12:28:35 +0100

    Rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lepra2::Application.config.secret_token = 'c5b52e2e2cafba83920233a945c3ecb6d7a361278c95e6bed01edb7420e1ced3cdb347726b1f07b2a9c37b8ac66a7d0dd9a009e09ad8538c00fa17d2a31a24d9'
