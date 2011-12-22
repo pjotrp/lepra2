b959a04fc7e689ac3fe95b55d7cb4a8c63f85cfa
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-22 10:22:23 +0100

    pershist

--- db/migrate/20111222091657_create_personal_histories.rb
+++ db/migrate/20111222091657_create_personal_histories.rb
@@ -2,8 +2,11 @@ class CreatePersonalHistories < ActiveRecord::Migration
   def change
     create_table :personal_histories do |t|
       t.string :occupation
+      t.string :education
       t.integer :guardian
       t.integer :address
+      t.integer :members # house members
+      t.integer :income  # household income
       t.integer :remark
 
       t.timestamps
