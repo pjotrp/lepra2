2102d1c2ca0769b6b37a554d87ff35fb361ef129
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-24 10:58:27 +0100

    Model

--- db/migrate/20111222083119_create_addresses.rb
+++ db/migrate/20111222083119_create_addresses.rb
@@ -1,13 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
-      t.string  :road
-      t.string  :village
+      t.string  :road,     :null => false
+      t.string  :village,  :null => false
       t.string  :postcode
       t.string  :phone
-      t.integer :location   # village/distict/gps
-      t.string  :latitude   # override location
-      t.string  :longitude  # override location
+      t.integer :location, :null => false   # village/distict/coordinates
+      t.string  :latitude                   # override location
+      t.string  :longitude                  # override location
       t.text    :remark    
 
       t.timestamps
