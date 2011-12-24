0ac8660d0bd3c515bb6fb036202cb2b7533ad858
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-24 10:33:32 +0100

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,79 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222083119) do
+ActiveRecord::Schema.define(:version => 20111222092909) do
 
   create_table "addresses", :force => true do |t|
     t.string   "road"
     t.string   "village"
     t.string   "postcode"
+    t.string   "phone"
     t.integer  "location"
-    t.integer  "district"
+    t.string   "latitude"
+    t.string   "longitude"
     t.text     "remark"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clinics", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", :force => true do |t|
+    t.boolean  "reaction"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "village"
+    t.string   "district"
+    t.string   "union"
+    t.string   "upozilla"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "people", :force => true do |t|
+    t.string   "registration"
+    t.string   "name"
+    t.integer  "year_of_birth"
+    t.boolean  "gender"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personal_histories", :force => true do |t|
+    t.string   "occupation"
+    t.string   "education"
+    t.integer  "guardian"
+    t.integer  "address"
+    t.integer  "members"
+    t.integer  "income"
+    t.integer  "remark"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reactions", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "remarks", :force => true do |t|
+    t.integer  "reporter"
+    t.text     "remark"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "staffs", :force => true do |t|
+    t.string   "name"
+    t.integer  "clinic"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
