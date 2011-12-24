26685227e49ad901a1f84a364557555eab00153f
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-24 11:31:42 +0100

    YoB

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ActiveRecord::Base
+  # validates :year_of_birth, :length => { :in => 1950..Time.now.year }
+  YEAR = Time.now.year
+  YEAR1 = YEAR - 90
+  validates_inclusion_of :year_of_birth, :in => YEAR1..YEAR, :message => "can only be between #{YEAR1} and #{YEAR}."
 end
--- db/migrate/20111222090906_create_people.rb
+++ db/migrate/20111222090906_create_people.rb
@@ -1,9 +1,9 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-      t.string :registration
-      t.string :name
-      t.integer :year_of_birth
+      t.string :registration, :null => false, :unique => true  # clinic/num/YYYY
+      t.string :name, :null => false
+      t.integer :year_of_birth, :null => false
       t.boolean :gender
 
       t.timestamps
