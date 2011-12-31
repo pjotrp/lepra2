f0eace68411900227d8bd5b708bb632268b10132
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-31 15:40:28 +0100

    Website

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.1.3'
 gem 'mysql2'
 gem 'devise'
 gem 'rails_admin', :git => 'git://github.com/sferik/rails_admin.git'
+gem 'haml'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -76,7 +76,7 @@ Lepra2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'lepra2#index'
 
   # See how all your routes lay out with "rake routes"
 
