692b4c314987795122a6ab2901044978b445a998
Pjotr Prins <pjotr.public01@thebird.nl>
2012-01-01 12:28:17 +0100

    formtastic with sass

--- app/models/person.rb
+++ app/models/person.rb
@@ -23,6 +23,6 @@ class Person < ActiveRecord::Base
   # validates :year_of_birth, :length => { :in => 1950..Time.now.year }
   validates_numericality_of :year_of_birth, :only_integer => true, :message => "can only be whole number."
   YEAR = Time.now.year
-  YEAR1 = YEAR - 90
+  YEAR1 = YEAR - 105
   validates_inclusion_of :year_of_birth, :in => YEAR1..YEAR, :message => "can only be between #{YEAR1} and #{YEAR}."
 end
