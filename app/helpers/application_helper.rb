5a4426ad5e350567d7f7227a1b5a2814a03e766d
Pjotr Prins <pjotr.public01@thebird.nl>
2012-01-15 17:27:54 +0100

    form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,54 @@
 module ApplicationHelper
 
-  def f_text f, field
+  def tr_text f, field, *opts
     html = <<EOP
       <tr>
         <td>
           #{f.label field}
         </td>
         <td>
-          #{f.text_field field}
+          #{f.text_field field, *opts}
         </td>
       </tr>
 EOP
     html
   end
-  def f_number f, field
+
+  def tr_number f, field, *opts
     html = <<EOP
       <tr>
         <td>
           #{f.label field}
         </td>
         <td>
-          #{f.number_field field}
+          #{f.number_field field, *opts}
+        </td>
+      </tr>
+EOP
+    html
+  end
+
+  def tr_check_box f, field, *opts
+    html = <<EOP
+      <tr>
+        <td>
+          #{f.label field}
+        </td>
+        <td>
+          #{f.check_box field, *opts}
+        </td>
+      </tr>
+EOP
+    html
+  end
+  def tr_date f, field, *opts
+    html = <<EOP
+      <tr>
+        <td>
+          #{f.label field}
+        </td>
+        <td>
+          #{f.text_field(field,*opts)}
         </td>
       </tr>
 EOP
