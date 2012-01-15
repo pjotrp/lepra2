4db69e0b5ec5eacaf0b9f0a4be8cbf87a7f17309
Pjotr Prins <pjotr.public01@thebird.nl>
2012-01-15 11:39:13 +0100

    Automated fields for form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def f_text f, field
+    html = <<EOP
+      <tr>
+        <td>
+          #{f.label field}
+        </td>
+        <td>
+          #{f.text_field field}
+        </td>
+      </tr>
+EOP
+    html
+  end
+  def f_number f, field
+    html = <<EOP
+      <tr>
+        <td>
+          #{f.label field}
+        </td>
+        <td>
+          #{f.number_field field}
+        </td>
+      </tr>
+EOP
+    html
+  end
 end
