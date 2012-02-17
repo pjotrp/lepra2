1dd3b1a112a9bbe6922615ec60598dac6e0ccd31
Pjotr Prins <pjotr.public01@thebird.nl>
2012-02-17 12:29:34 +0100

    Testing for search

--- spec/views/lepra2/people/index.html.haml_spec.rb
+++ spec/views/lepra2/people/index.html.haml_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 describe "people/index.html.haml" do
-it "displays the front page" do
-  @people = []
-  @people = Person.search()
-  render
-  rendered.should contain("Clinic")
+  it "displays the front page" do
+    @people = Person.search("","")  # params[:search], params[:is_anywhere], params[:clinic_id])
+    render
+    rendered.should contain("Clinic")
+    rendered.should contain("0 matches")
+  end
+
+  it "should one hit after adding a person" do
+    person = stub_model(Person,
+      :name => "Test User"
+    )
+
+    # @people = Person.search("","")  # params[:search], params[:is_anywhere], params[:clinic_id])
+    @people = [ person ]
+    render
+    rendered.should contain("Clinic")
+    rendered.should contain("1 matches")
+    rendered.should contain("Test User")
   end
 end
