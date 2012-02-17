ec004ea8c290b7016ed9b0bfb4a853af0dab2cc4
Pjotr Prins <pjotr.public01@thebird.nl>
2012-02-17 12:14:57 +0100

    Two view tests pass

--- spec/views/lepra2/people/index.html.haml_spec.rb
+++ spec/views/lepra2/people/index.html.haml_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 describe "people/index.html.haml" do
 it "displays the front page" do
   @people = []
+  @people = Person.search()
   render
   rendered.should contain("Clinic")
   end
