11ea26ed1f1ba3911e3a2e3e237106e05ef6a0d8
Pjotr Prins <pjotr.public01@thebird.nl>
2012-05-13 11:30:22 +0200

    Adding test

---
+++ test/functional/personal_histories_controller_test.rb
@@ -0,0 +1,15 @@
+# ruby -I test test/functional/personal_histories_controller_test.rb 
+
+require 'test_helper'
+
+class PersonalHistoriesControllerTest < ActionController::TestCase
+  test "create a new personal history" do
+    post :create, :personal_history => 
+      { :date => '10/10/2010', :registration => '112', :registration_date => '10/10/2010',
+        :staff_id => 1 }
+    personal_history = assigns(:personal_history)
+    assert_not_nil personal_history
+    assert personal_history.errors.count == 0
+    assert_equal "Personal history was successfully created.", flash[:notice]
+  end
+end
